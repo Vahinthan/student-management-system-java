32ebf5c5d9928e45f8958bbb53f5188959ffd889
Vahinthan <raseetharanvahinthan2003@gmail.com>
2026-01-09 23:58:26 +0530

    Add Main Java class

---
+++ src/Main.java
@@ -0,0 +1,6 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Student Management System");
+        System.out.println("Project by Vahinthan");
+    }
+}
