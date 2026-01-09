29ba7434d38aa755228fb7c3111f2acfb7a5d6f2
Vahinthan <raseetharanvahinthan2003@gmail.com>
2026-01-10 00:18:38 +0530

    Use ArrayList to store students

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,20 @@
+import java.util.ArrayList;
+
 public class Main {
     public static void main(String[] args) {
-        Student s1 = new Student(1, "Kamal", 20);
-        Student s2 = new Student(2, "Nimal", 21);
 
-        System.out.println(s1);
-        System.out.println(s2);
+        // Create ArrayList to store students
+        ArrayList<Student> students = new ArrayList<>();
+
+        // Add students
+        students.add(new Student(1, "Kamal", 20));
+        students.add(new Student(2, "Nimal", 21));
+        students.add(new Student(3, "Saman", 22));
+
+        // Display all students
+        System.out.println("Student List:");
+        for (Student s : students) {
+            System.out.println(s);
+        }
     }
 }
